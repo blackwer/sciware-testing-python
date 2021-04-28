597ad19062cc59a68824add97da6a02dcf82a15e
Robert Blackwell <rblackwell@flatironinstitute.org>
2021-04-28 16:31:22 -0400

    Pass exercises

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,14 +1,24 @@
 import pytest
 import sciware_testing_python
 
+
 def test_sum1():
     # basic sanity test: do we get the right answer for a simple case?
-    assert True #sciware_testing_python.sum_numbers([1,2,3]) == ?
+    assert sciware_testing_python.sum_numbers([1, 2, 3]) == 6
+
 
 def test_sum2():
     # what's the sum of an empty list
-    pass
+    assert sciware_testing_python.sum_numbers([]) == 0
 
-#@pytest.mark.xfail(strict=True)
+
 def test_sum_bools():
-    pass
+    try:
+        sciware_testing_python.sum_numbers([False, True])
+    except ValueError:
+        return True
+    return False
+
+
+def test_add_vectors():
+    assert sciware_testing_python.add_vectors([1, 2], [2, 3]) == [3, 5]
